9ee11747878b5e8471df9c7b52cca24a6a4dd5f9
Borzhemskii A <felon@deeplay.io>
2022-11-18 13:33:02 +0800

    добавление API

--- src/test/java/API/ApiTests.java
+++ src/test/java/API/ApiTests.java
@@ -6,8 +6,7 @@ import API.utils.ExpectedResponse;
 import API.utils.UserGenerator;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 
 import java.util.Base64;
 
@@ -15,11 +14,14 @@ import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ApiTests {
     private static String basicUsername = "front_2d6b0a8391742f5d789d7d915755e09e:";
-    private static String token = Base64.getUrlEncoder().encodeToString(basicUsername.getBytes());
-    private String access_token;
+    private static String basic_token = Base64.getUrlEncoder().encodeToString(basicUsername.getBytes());
+    private String guest_access_token;
+    private String user_access_token;
     private CreateUserRequest user = UserGenerator.getUser();
+    private CreateUserResponse userData;
     private int user_id;
 
     private static final RequestSpecification REQ_SPEC =
@@ -29,27 +31,30 @@ public class ApiTests {
                     .build();
 
     @Test
+    @Order(1)
     public void getGuestToken(){
-         this.access_token = given()
+         guest_access_token = given()
                  .spec(REQ_SPEC)
-                 .header("Authorization", "Basic " + token)
+                 .header("Authorization", "Basic " + basic_token)
                  .body("{\"grant_type\":\"client_credentials\", \"scope\":\"guest:default\"}")
                  .when().post("/v2/oauth2/token")
                  .then()
                  .assertThat().statusCode(200)
                  .extract().path("access_token");
 
-         assertThat(access_token).isNotNull();
+         assertThat(guest_access_token).isNotNull();
+         System.out.println("Получен токен гостя:\n" + guest_access_token);
     }
 
-    @Test()
+    @Test
+    @Order(2)
     public void registerUser(){
          CreateUserResponse rs = given()
                  .spec(REQ_SPEC)
-                 .header("Authorization", "Bearer " + access_token)
-                 .body(user).log().all()
+                 .header("Authorization", "Bearer " + guest_access_token)
+                 .body(user)
                  .when().post("/v2/players")
-                 .then().log().all()
+                 .then()
                  .assertThat().statusCode(201)
                  .extract().as(CreateUserResponse.class);
 
@@ -57,8 +62,53 @@ public class ApiTests {
          exrs.setId(rs.getId());
          assertThat(rs).isEqualTo(exrs);
          this.user_id = rs.getId();
+
+        System.out.println("Пользователь с id " + user_id + " успешно зарегестрирован");
+        userData = rs;
     }
 
+    @Test
+    @Order(3)
+    public void authorizationUser(){
+        user_access_token = given()
+                .spec(REQ_SPEC)
+                .header("Authorization", "Basic " + basic_token)
+                .body("{\"grant_type\":\"password\", \"username\":\"" + user.getUsername() + "\", \"password\":\"" + user.getPassword_change() + "\"}").log().all()
+                .when().post("/v2/oauth2/token")
+                .then()
+                .assertThat().statusCode(200)
+                .extract().path("access_token");
 
+        assertThat(user_access_token).isNotNull();
+        System.out.println("Получен токен пользователя:\n" + user_access_token);
+    }
+
+    @Test
+    @Order(4)
+    public void getUserProfile(){
+        CreateUserResponse rs = given()
+                .spec(REQ_SPEC)
+                .header("Authorization", "Bearer " + user_access_token)
+                .when().get("/v2/players/" + user_id)
+                .then()
+                .assertThat().statusCode(200)
+                .extract().as(CreateUserResponse.class);
+
+        assertThat(rs).isEqualTo(userData);
+        System.out.println("Данные пользователя совпадают с ожидаемыми");
+    }
+
+    @Test
+    @Order(5)
+    public void getAnotherUserProfile(){
+        given()
+                .spec(REQ_SPEC)
+                .header("Authorization", "Bearer " + user_access_token)
+                .when().get("/v2/players/20" + user_id)
+                .then()
+                .assertThat().statusCode(404);
+
+        System.out.println("Данные другого пользователя не найдены");
+    }
 
 }
